a9e3fd0d006caf8ca35109eb63e6f8ceead4d9b4
AnimacionLosEnlaces <animacionlosenlaces@gmail.com>
2017-12-19 09:54:55 +0100

    Primer commit
    Creado index.php
    Añadida imagen Koala

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Repositorio Git Hub</title>
+</head>
+
+<body>
+<?php
+
+echo "HOLA MUNDO";
+
+?>
+<hr>
+
+<img src="img/Koala.jpg">
+</body>
+</html>
